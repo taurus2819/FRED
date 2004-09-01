35f676f75b05120226d77875bffce86480204397
Old User <developer@gns.cri.nz>
2004-09-01 09:31:55 +0000

    *** empty log message ***
    git-svn-id: file:///repo/applications/FRED/trunk@9204 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/dataentry/LocalityDE.java
+++ nz/cri/gns/fred/dataentry/LocalityDE.java
@@ -137,6 +137,7 @@ public abstract class LocalityDE implements DataEntryForm {
 		}
 		fields[field] = value;
 		savedFlag = false;
+		System.out.println("Field " + field + " set OK");
 	}
 
 	public String getField(int field) {
@@ -358,7 +359,7 @@ public abstract class LocalityDE implements DataEntryForm {
 			String north = coord.substring(coord.indexOf("*") + 1, coord.length());
 			try {
 				origCoord =	new NorthingEasting(Double.parseDouble(north), Double.parseDouble(east));
-				origSystem = DatumFactory.createNZMG();
+				origSystem = DatumFactory.createNZMS260();
 				countryCode = "NZ";
 			} catch (Exception e) {
 				throw new DataInputException("Coordinate", "Invalid value");
@@ -371,7 +372,7 @@ public abstract class LocalityDE implements DataEntryForm {
 			String north = coord.substring(coord.lastIndexOf("*") + 1, coord.length());
 			try {
 				origCoord = new TruncNorthingEasting(Double.parseDouble(north), Double.parseDouble(east), sheet, east.length());
-				origSystem = DatumFactory.createNZMS260();
+				origSystem = DatumFactory.createTruncNZMS260();
 				countryCode = "NZ";
 			} catch (Exception e) {
 				throw new DataInputException("Coordinate", "Invalid value");
