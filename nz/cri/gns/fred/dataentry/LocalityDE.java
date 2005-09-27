79d607268af5675b95dabf422f1b22aadc80afc4
Old User <developer@gns.cri.nz>
2005-09-27 20:32:25 +0000

    *** empty log message ***
    git-svn-id: file:///repo/applications/FRED/trunk@9705 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/dataentry/LocalityDE.java
+++ nz/cri/gns/fred/dataentry/LocalityDE.java
@@ -376,7 +376,6 @@ public abstract class LocalityDE implements DataEntryForm {
 			out.write("<td>Locality</td>");
 			out.write("<td>" + FREDUtils.noNulls(feature.getAsString(Feature.WORKING_FOLDER_ID)) + "</td>");
 			out.write("<td>" + FREDUtils.noNulls(feature.getAsString(Feature.STATUS)) + "</td>");
-			out.write("<td>Testing</td>");
 			out.write("<td>" + featureType + "</td>");
 			out.write("<td>" + FREDUtils.noNulls(getFieldForHTML(FIELD_NUMBER)) + "</td>");
 			out.write("<td>" + FREDUtils.noNulls(getFieldForHTML(REGISTRATION_AREA)) + "</td>");
