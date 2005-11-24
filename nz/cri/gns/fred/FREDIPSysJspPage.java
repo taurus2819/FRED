e20f53ff9a1a4ef2f6e9d8bb9d9043a7528e0f37
Old User <developer@gns.cri.nz>
2005-11-24 23:54:51 +0000

    testing new ExtranetTemplate
    git-svn-id: file:///repo/applications/FRED/trunk@10037 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/FREDIPSysJspPage.java
+++ nz/cri/gns/fred/FREDIPSysJspPage.java
@@ -61,7 +61,7 @@ public abstract class FREDIPSysJspPage extends IPSysJspPage {
 		links[3] = new KeyValueObject("/fred/folder_list.jsp", "Data Entry");
 		et.setLinks(links);
 		et.setImageBase("/fred/images/fred.gif");
-		//et.setNewHeaderStyle(true);
+		et.setNewHeaderStyle(true);
 		et.setAppLogo(new KeyValueObject(null, "/fred/images/gsnz_logo.gif"));
 		KeyValueObject logos[] = new KeyValueObject[2];
 		logos[0] = new KeyValueObject("http://www.gsnz.org.nz", "/fred/images/gsnz_logo.gif");
