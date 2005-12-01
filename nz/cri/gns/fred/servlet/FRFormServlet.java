a141f66ab9ccc10e75a70431709afa1901ee3fb0
Old User <developer@gns.cri.nz>
2005-12-01 00:40:34 +0000

    *** empty log message ***
    git-svn-id: file:///repo/applications/FRED/trunk@10106 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/servlet/FRFormServlet.java
+++ nz/cri/gns/fred/servlet/FRFormServlet.java
@@ -90,7 +90,7 @@ public class FRFormServlet extends HttpServlet {
 
 		PdfPTable table = new PdfPTable(2);
 		table.setWidthPercentage(100);
-		table.setWidths(new float[] {100, table.getTotalWidth() - 100});
+		//table.setWidths(new float[] {100, table.getTotalWidth() - 100});
 		table.setSpacingAfter(7 * MM_TO_PT);
 				
 		//Logos
@@ -122,6 +122,7 @@ public class FRFormServlet extends HttpServlet {
 		headerTable.addCell(cell);			
 			
 		table.addCell(headerTable);
+		
 		document.add(table);
 		
 		//Masterfile text
@@ -146,7 +147,6 @@ public class FRFormServlet extends HttpServlet {
 		
 		table.addCell(cell);
 		
-			
 		document.add(table);
 			
 					
