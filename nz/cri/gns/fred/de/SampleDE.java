a73f69fa56c94e7357f8b023c07d6ac36c62fe60
Old User <developer@gns.cri.nz>
2005-12-01 21:59:33 +0000

    bug fix - stop newline character being saved at end of "sent to" comments
    git-svn-id: file:///repo/applications/FRED/trunk@10138 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/de/SampleDE.java
+++ nz/cri/gns/fred/de/SampleDE.java
@@ -741,7 +741,8 @@ public class SampleDE extends DETemplate implements DataEntryForm {
     			if (parts[2].length() > 0 && lab == null)
     				error.add(new String[] {"Sent To", "Invalid lab: " + parts[2]});
     			
-    			String comments = parts[3].replaceAll("\\n", null);
+    			String comments = parts[3];
+    			System.out.println("*" + comments + "*" + " is comments null: " + (comments == null));
     			
     			sentToSet.add(sampleUtil.findOrCreateSentTo(sample, group, person, lab, comments));
     		} catch (Exception e) {
