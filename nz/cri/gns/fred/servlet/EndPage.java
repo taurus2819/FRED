a20758659e5616506ade75e2c55650d7c9708191
Old User <developer@gns.cri.nz>
2005-12-05 02:10:17 +0000

    format footer
    git-svn-id: file:///repo/applications/FRED/trunk@10191 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/servlet/EndPage.java
+++ nz/cri/gns/fred/servlet/EndPage.java
@@ -1,8 +1,13 @@
 package nz.cri.gns.fred.servlet;
 
+import nz.cri.gns.fred.util.PDFUtil;
+
 import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfPageEventHelper;
 import com.lowagie.text.pdf.PdfWriter;
@@ -12,19 +17,19 @@ public class EndPage extends PdfPageEventHelper {
 	private static final float MM_TO_PT = 2.8346f;
 	
 	public void onEndPage(PdfWriter writer, Document document) {
+		//footer
 		Rectangle page = document.getPageSize();
-		PdfPTable foot = new PdfPTable(1);
-		foot.addCell("Printed from FRED database on " + new java.util.Date());
-		foot.setTotalWidth(page.width() - document.leftMargin() - document.rightMargin());
-		foot.writeSelectedRows(0, -1, document.leftMargin(), document.bottomMargin(),
-				writer.getDirectContent());
+		PdfPTable footer = new PdfPTable(1);
+		PDFUtil.addCell(footer, "Generated from FRED database on " + new java.util.Date(), FontFactory.getFont(FontFactory.HELVETICA, 8, Font.BOLD), PdfPCell.ALIGN_RIGHT, 1);
+		footer.setTotalWidth(page.width() - document.leftMargin() - document.rightMargin());
+		footer.writeSelectedRows(0, -1, document.leftMargin(), document.bottomMargin(),	writer.getDirectContent());
         
-		//rectangle
+		//border
 		PdfContentByte cb = writer.getDirectContent();
 		cb.saveState();
 		cb.setRGBColorStroke(110, 110, 110);
 		cb.setLineWidth(2);
-		cb.rectangle(20 * MM_TO_PT, 10 * MM_TO_PT, 180 * MM_TO_PT, 277 * MM_TO_PT);
+		cb.rectangle(15 * MM_TO_PT, 10 * MM_TO_PT, 185 * MM_TO_PT, 277 * MM_TO_PT);
 		cb.stroke();
 		cb.restoreState();
     }
