3797efa57ca99fecdc7170d1d69acc933cd5baf9
Old User <developer@gns.cri.nz>
2006-02-02 22:47:19 +0000

    de-bugging
    git-svn-id: file:///repo/applications/FRED/trunk@10559 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/util/AuditUtil.java
+++ nz/cri/gns/fred/util/AuditUtil.java
@@ -67,16 +67,18 @@ public class AuditUtil extends ModelUtil implements FREDConstants, AuditedUtil {
     
     public static String getAuditBacklogStatus(Audit audit) {
     	System.out.println("** Checking Backlog Audit Status for auditID: " + audit.getAuditId());
+    	System.out.println("Audit Created Date: " + audit.getCreatedDate());
 		if (audit.getStatus().equals(FREDConstants.APPROVED)) {
 			if (audit.getCuratorComments() != null && audit.getCuratorComments().indexOf("backlog") > 0) {
 				System.out.println("BACKLOG_COMPLETE");
 				return FREDConstants.BACKLOG_COMPLETE;
 			}
 			try {
-				if (audit.getCreatedDate() != null && audit.getCreatedDate().after(new SimpleDateFormat("dd/MM/yyyy").parse("01/10/2005"))); {
-					System.out.println("Audit Created Date: " + audit.getCreatedDate());
-					System.out.println("BACKLOG_NEW");
-					return FREDConstants.BACKLOG_NEW;
+				if (audit.getCreatedDate() != null) {
+					if (audit.getCreatedDate().after(new SimpleDateFormat("dd/MM/yyyy").parse("01/10/2005"))); {
+						System.out.println("BACKLOG_NEW");
+						return FREDConstants.BACKLOG_NEW;
+					}
 				}
 					
 			} catch (ParseException e) {
