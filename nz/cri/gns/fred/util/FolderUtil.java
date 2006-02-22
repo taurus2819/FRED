9c64a48a78008053b8bce946d55a75765eb65a4e
Old User <developer@gns.cri.nz>
2006-02-22 01:55:07 +0000

    tidy up imports
    git-svn-id: file:///repo/applications/FRED/trunk@10595 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/util/FolderUtil.java
+++ nz/cri/gns/fred/util/FolderUtil.java
@@ -9,7 +9,6 @@ import nz.cri.gns.dataaccess.StorageAccessException;
 import nz.cri.gns.fred.dao.DAOFactory;
 import nz.cri.gns.fred.dao.FolderDAO;
 import nz.cri.gns.fred.dao.FolderTypeDAO;
-import nz.cri.gns.fred.model.Feature;
 import nz.cri.gns.fred.model.Folder;
 import nz.cri.gns.fred.model.FolderAccessor;
 import nz.cri.gns.fred.model.FolderRight;
