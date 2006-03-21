1e3448b6b04c0333b01575ed24747e51b59e4dac
Old User <developer@gns.cri.nz>
2006-03-21 23:10:37 +0000

    de-bugging
    git-svn-id: file:///repo/applications/FRED/trunk@10681 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/de/SampleDE.java
+++ nz/cri/gns/fred/de/SampleDE.java
@@ -726,8 +726,8 @@ public class SampleDE extends DETemplate implements DataEntryForm {
 		//Collectors
         PersonUtil personUtil = new PersonUtil(factory);
         try {
-        	System.out.println("Collectors parameter: " + request.getParameter("Coll"));
         	String[] collectors = request.getParameterValues("Coll");
+        	System.out.println("Collectors.length: " + collectors.length);
         	//This is to support the spreadsheet
         	if (collectors.length <= 1)
         		sample.setCollectors(FREDUtil.getPersons(request.getParameter("Coll"), personUtil, "Collectors", addIfNew));
