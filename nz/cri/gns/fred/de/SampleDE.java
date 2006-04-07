6545ecaeade803cf1fa97b93e5783ad6ae5b4417
Old User <developer@gns.cri.nz>
2006-04-07 02:25:33 +0000

    Fixed idiocy
    git-svn-id: file:///repo/applications/FRED/trunk@10775 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/de/SampleDE.java
+++ nz/cri/gns/fred/de/SampleDE.java
@@ -635,7 +635,7 @@ public class SampleDE extends DETemplate implements DataEntryForm {
 		template.addSub("inputField", "CollDate");
 		template.addSub("button", "CollDateCal");
 		template.loadAll(out);
-		Template template = provider.getContent("sample_postform.html");
+		template = provider.getContent("sample_postform.html");
 		template.loadAll(out);
 	}
 
