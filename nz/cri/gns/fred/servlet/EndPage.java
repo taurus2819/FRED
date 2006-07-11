bc5c876d194c39a8468ef8376f115876185a979f
Old User <developer@gns.cri.nz>
2006-07-11 00:11:40 +0000

    adding page numbering
    git-svn-id: file:///repo/applications/FRED/trunk@10924 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/servlet/EndPage.java
+++ nz/cri/gns/fred/servlet/EndPage.java
@@ -11,11 +11,13 @@ import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfPageEventHelper;
+import com.lowagie.text.pdf.PdfTemplate;
 import com.lowagie.text.pdf.PdfWriter;
 
 public class EndPage extends PdfPageEventHelper {
 
 	private static final float MM_TO_PT = 2.8346f;
+	private PdfTemplate tpl;
 	
 	public void onEndPage(PdfWriter writer, Document document) {
 		//footer
@@ -35,6 +37,7 @@ public class EndPage extends PdfPageEventHelper {
 		PDFUtil.addCell(footer, "Printed on " + new java.util.Date() + " from FRED, the computer database for the NZ Fossil Record File (FRF).", FontFactory.getFont(FontFactory.HELVETICA, 7, Font.BOLD), PdfPCell.ALIGN_LEFT, 2);
 		PDFUtil.addCell(footer, "FRF is a nationally significant database administered by GSNZ and GNS Science", FontFactory.getFont(FontFactory.HELVETICA, 7, Font.BOLD), PdfPCell.ALIGN_LEFT, 1);
 		PDFUtil.addCell(footer, "Page " + writer.getPageNumber() + " of ", FontFactory.getFont(FontFactory.HELVETICA, 7, Font.NORMAL), PdfPCell.ALIGN_RIGHT, 1);
+		writer.getDirectContent().addTemplate(tpl, document.rightMargin(), document.bottomMargin());
 		footer.writeSelectedRows(0, -1, document.leftMargin(), document.bottomMargin(),	writer.getDirectContent());
         
 		//border
@@ -46,5 +49,14 @@ public class EndPage extends PdfPageEventHelper {
 		cb.stroke();
 		cb.restoreState();
     }
+	
+    public void onCloseDocument(PdfWriter writer, Document document) {
+        tpl.beginText();
+        tpl.setFontAndSize(FontFactory.getFont(FontFactory.HELVETICA, 7, Font.NORMAL).getBaseFont(), 7);
+        tpl.setTextMatrix(0, 0);
+        tpl.showText("" + (writer.getPageNumber() - 1));
+        tpl.endText();
+     }
+ }
 
 }
