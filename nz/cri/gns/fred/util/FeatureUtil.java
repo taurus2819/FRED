81ff2c745428595d8645980f8668762b07c38982
Old User <developer@gns.cri.nz>
2006-07-24 08:51:44 +0000

    sorted parseFrNumber
    git-svn-id: file:///repo/applications/FRED/trunk@10991 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/util/FeatureUtil.java
+++ nz/cri/gns/fred/util/FeatureUtil.java
@@ -873,7 +873,6 @@ public class FeatureUtil extends ModelUtil implements AuditedUtil {
 			String serialNumStr = String.valueOf(serialNumber);
 			while (serialNumStr.length() < 4)
 				serialNumStr = "0" + serialNumStr;
-			System.out.println("parsed FRNumber = " + mapSheet + "/f" + serialNumStr + ((recollectionNumber != null) ? recollectionNumber : ""));
 			FrNumber frNumber = featureDAO.getFrNumber(mapSheet + "/f" + serialNumStr + ((recollectionNumber != null) ? recollectionNumber : ""));
 			if (frNumber == null && createNew) {
 				frNumber = new nz.cri.gns.fred.hibernate.FrNumber();
