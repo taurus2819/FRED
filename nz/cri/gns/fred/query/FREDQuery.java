2c1168e28c4e41eb6532e1f899ef5139c0452071
Old User <developer@gns.cri.nz>
2006-10-24 09:05:26 +0000

    try joins
    git-svn-id: file:///repo/applications/FRED/trunk@11172 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/query/FREDQuery.java
+++ nz/cri/gns/fred/query/FREDQuery.java
@@ -44,7 +44,7 @@ public class FREDQuery extends HqlQuery implements NumberSource {
 		add(new TwoLevelField("Drillhole Fields", f));
 		
 		f = new Field[2];
-		f[0] = new HqlTableRequiredTextField("sample.significance", "Significance/Comments", "Sample AS sample", new HqlAliasedJoin("f", "sample", "sample"));
+		f[0] = new HqlTableRequiredTextField("s.significance", "Significance/Comments", "Sample AS s", new HqlAliasedJoin("f", "samples", "s"));
 		f[1] = new BasicTextField("f.samples.siginificance", "Sig2");
 		add(new TwoLevelField("Sample Fields", f));
 		              
