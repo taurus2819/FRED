dc3124b7fe4938a6e53c1238a34b5414318af174
Old User <developer@gns.cri.nz>
2007-01-03 23:44:09 +0000

    add getList method
    git-svn-id: file:///repo/applications/FRED/trunk@11353 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/dao/SampleDAO.java
+++ nz/cri/gns/fred/dao/SampleDAO.java
@@ -200,4 +200,7 @@ public interface SampleDAO {
 	public void delete(SentTo sentTo) throws StorageAccessException;
 	
 	public void delete(Relationship rel) throws StorageAccessException;
+	
+	public <T extends Comparable<? super T>> List<T> getList(String query, Class<T> clazz, Object ... parameters) throws StorageAccessException;
+
 }
--- nz/cri/gns/fred/dao/TaxonomicDAO.java
+++ nz/cri/gns/fred/dao/TaxonomicDAO.java
@@ -1,5 +1,7 @@
 package nz.cri.gns.fred.dao;
 
+import java.util.List;
+
 import nz.cri.gns.dataaccess.StorageAccessException;
 import nz.cri.gns.fred.model.PaleontologyListEntry;
 import nz.cri.gns.fred.model.Taxon;
@@ -51,4 +53,6 @@ public interface TaxonomicDAO {
      */
 	public Taxon getTaxon(TaxonomicGroup taxonomicGroup, String name) throws StorageAccessException;
 
+	public <T extends Comparable<? super T>> List<T> getList(String query, Class<T> clazz, Object ... parameters) throws StorageAccessException;
+	
 }
