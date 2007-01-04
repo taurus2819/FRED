f7ee4a1a2886be19ec52337ac1ab2e27c80e0833
Old User <developer@gns.cri.nz>
2007-01-04 01:19:54 +0000

    extend Comparable and NameableAndIdentifiable
    git-svn-id: file:///repo/applications/FRED/trunk@11357 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/hibernate/StratigraphicUnit.java
+++ nz/cri/gns/fred/hibernate/StratigraphicUnit.java
@@ -1,27 +1,47 @@
 package nz.cri.gns.fred.hibernate;
 
-public class StratigraphicUnit implements
-		nz.cri.gns.fred.model.StratigraphicUnit {
+import java.io.Serializable;
 
+public class StratigraphicUnit implements Serializable, nz.cri.gns.fred.model.StratigraphicUnit {
+
+    private static final long serialVersionUID = 20050818L;
+    
 	private Integer id;
 	private String name;
 	
-	public StratigraphicUnit() {
-	}
-
-	public void setId(Integer id) {
+	public StratigraphicUnit(Integer id, String name) {
 		this.id = id;
-	}
-
-	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public StratigraphicUnit() {
 	}
 
 	public Integer getId() {
 		return id;
 	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int compareTo(nz.cri.gns.fred.model.StratigraphicUnit arg0) {
+		return name.compareTo(arg0.getName());
+	}
+
+	public String getUniqueIdentifier() {
+		return String.valueOf(id);
+	}
+
+	public String getDisplayName() {
 		return name;
 	}
 
--- nz/cri/gns/fred/model/StratigraphicUnit.java
+++ nz/cri/gns/fred/model/StratigraphicUnit.java
@@ -1,8 +1,8 @@
 package nz.cri.gns.fred.model;
 
-public interface StratigraphicUnit {
+import nz.cri.gns.core.NameableAndIdentifiable;
 
+public interface StratigraphicUnit extends Comparable<StratigraphicUnit>, NameableAndIdentifiable {
 	public Integer getId();
-	
 	public String getName();
 }
