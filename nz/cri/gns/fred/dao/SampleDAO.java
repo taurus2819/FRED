9ac9d35427ad0e08b3848367967f7e53d8b08ca3
Old User <developer@gns.cri.nz>
2007-01-04 23:36:58 +0000

    merge TaxonomicGroupDAO into TaxonomicDAO
    git-svn-id: file:///repo/applications/FRED/trunk@11374 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- nz/cri/gns/fred/dao/SampleDAO.java
+++ nz/cri/gns/fred/dao/SampleDAO.java
@@ -205,6 +205,6 @@ public interface SampleDAO {
 	
 	public void delete(Relationship rel) throws StorageAccessException;
 	
-	public <T extends Comparable> List<T> getList(String query, Class<T> clazz, Object ... parameters) throws StorageAccessException;
+	public <T extends Comparable<? super T>> List<T> getList(String query, Class<T> clazz, Object ... parameters) throws StorageAccessException;
 
 }
