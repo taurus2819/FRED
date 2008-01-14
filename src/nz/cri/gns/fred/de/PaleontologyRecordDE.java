b2da3784111a2613fecb6594f44d9e42488d35ac
Old User <developer@gns.cri.nz>
2008-01-14 21:56:16 +0000

    *** empty log message ***
    git-svn-id: file:///repo/applications/FRED/trunk@11671 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- src/nz/cri/gns/fred/de/PaleontologyRecordDE.java
+++ src/nz/cri/gns/fred/de/PaleontologyRecordDE.java
@@ -295,6 +295,10 @@ public class PaleontologyRecordDE extends RecordDE {
 									+ entry.getTaxonomicName().replaceAll("'", "\\'") + "', '"
 									+ DBUtils.nvl((taxon == null) ? "" : taxon.getAuthor()).replaceAll("'", "\\'") + "', '"
 									+ DBUtils.nvl(TaxonomicUtil.encodeTaxaComments(entry)).replaceAll("'", "\\'") + "');");
+							System.out.println("addTaxa('" + group.getName() + ": "
+									+ entry.getTaxonomicName().replaceAll("'", "\\'") + "', '"
+									+ DBUtils.nvl((taxon == null) ? "" : taxon.getAuthor()).replaceAll("'", "\\'") + "', '"
+									+ DBUtils.nvl(TaxonomicUtil.encodeTaxaComments(entry)).replaceAll("'", "\\'") + "');");
 							//out.println(group.getName() + "*" 
 							//		+ entry.getTaxonomicName() + "*" 
 							//		+ DBUtils.nvl((taxon == null) ? "" : taxon.getAuthor()) + "*" 
