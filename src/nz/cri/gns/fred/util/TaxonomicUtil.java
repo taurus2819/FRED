0bc21d7cd28c3b772615f5fcf9b3e581f648dd92
Old User <developer@gns.cri.nz>
2008-02-20 08:48:07 +0000

    fixed
    git-svn-id: file:///repo/applications/FRED/trunk@11763 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- src/nz/cri/gns/fred/util/TaxonomicUtil.java
+++ src/nz/cri/gns/fred/util/TaxonomicUtil.java
@@ -375,10 +375,8 @@ public class TaxonomicUtil extends ModelUtil {
 			if (FREDUtil.isEmpty(specCoord))
 				try {
 					Integer.parseInt(comments);
-					System.out.println("Comments numeric");
 					return "||" + comments;
 				} catch (Exception e) {
-					e.printStackTrace();
 					return comments;
 				}
 			return "|" + specCoord + "|" + DBUtils.nvl(comments);
