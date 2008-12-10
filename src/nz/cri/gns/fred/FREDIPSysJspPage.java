8630ff4818f3f9c0f1ea77934c965543f3df29bd
Old User <developer@gns.cri.nz>
2008-12-10 00:35:24 +0000

    *** empty log message ***
    git-svn-id: file:///repo/applications/FRED/trunk@11920 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- src/nz/cri/gns/fred/FREDIPSysJspPage.java
+++ src/nz/cri/gns/fred/FREDIPSysJspPage.java
@@ -86,7 +86,7 @@ public abstract class FREDIPSysJspPage extends IPSysJspPage {
 		//set FRNumber lik
 		String htmlLink = "<img src=\"images\\blank.gif\" height=\"20\" width=\"10\" alt=\"\" /><form method=\"post\" action=\"locality\" name=\"FRNumJumpForm\" style=\"display: inline; margin: 0;\">"
 			+ "<input type=\"text\" size=\"10\" name=\"frNum\" style=\"border: 0; font-size: 10pt\" />&nbsp;"
-			+ "<input type=\"submit\" value=\"Go\" /></form><br />Enter FrNumber or a range";
+			+ "<input type=\"submit\" value=\"Go\" /></form><br /><img src=\"images\\blank.gif\" height=\"1\" width=\"10\" alt=\"\" />Enter FrNumber or a range";
 		et.setButtons(new Link[] {
 				new CustomHTMLLink(htmlLink)
 				, new IconnedLink("index.jsp", "images/home.gif", "FRED Home")
