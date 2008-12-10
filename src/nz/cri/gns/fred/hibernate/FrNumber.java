e28d0dc3073004779f3409021ca3f5b3602d79e8
Old User <developer@gns.cri.nz>
2008-12-10 00:35:31 +0000

    add toString
    git-svn-id: file:///repo/applications/FRED/trunk@11921 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- src/nz/cri/gns/fred/hibernate/FrNumber.java
+++ src/nz/cri/gns/fred/hibernate/FrNumber.java
@@ -5,71 +5,21 @@ import nz.cri.gns.fred.model.Sample;
 import java.io.Serializable;
 import java.util.Set;
 
-/** @author Hibernate CodeGenerator */
 public class FrNumber implements Serializable, nz.cri.gns.fred.model.FrNumber {
 
 	private static final long serialVersionUID = 20050818L;
 	
-    /** identifier field */
     private Integer frId;
-
-    /** persistent field */
     private String mapSheet;
-
-    /** persistent field */
     private Integer serialNumber;
-
-    /** nullable persistent field */
     private String recollectionNumber;
-
-    /** nullable persistent field */
     private String frnumComments;
-
-    /** nullable persistent field */
     private String frNumber;
-    
-    /** nullable persistent field */
     private String obsolete;
-    
-    /** persistent field */
     private Set<Feature> features;
-
-    /** persistent field */
     private Set<Feature> featuresByYard;
-    
-    /** persistent field */
     private Set<Sample> samples;
-    
-    /** persistent field */
     private Set<Sample> samplesByYard;
-
-    /** full constructor */
-    public FrNumber(String mapSheet, Integer serialNumber, String recollectionNumber, String frnumComments, String frNumber, String obsolete, Set<Feature> features, Set<Feature> featuresByYard, Set<Sample> samples, Set<Sample> samplesByYard) {
-        this.mapSheet = mapSheet;
-        this.serialNumber = serialNumber;
-        this.recollectionNumber = recollectionNumber;
-        this.frnumComments = frnumComments;
-        this.frNumber = frNumber;
-        this.obsolete = obsolete;
-        this.features = features;
-        this.featuresByYard = featuresByYard;
-        this.samples = samples;
-        this.samplesByYard = samplesByYard;
-    }
-
-    /** default constructor */
-    public FrNumber() {
-    }
-
-    /** minimal constructor */
-    public FrNumber(String mapSheet, Integer serialNumber, Set<Feature> features, Set<Feature> featuresByYard, Set<Sample> samples, Set<Sample> samplesByYard) {
-        this.mapSheet = mapSheet;
-        this.serialNumber = serialNumber;
-        this.features = features;
-        this.featuresByYard = featuresByYard;
-        this.samples = samples;
-        this.samplesByYard = samplesByYard;
-    }
 
     public Integer getFrId() {
         return this.frId;
@@ -163,11 +113,7 @@ public class FrNumber implements Serializable, nz.cri.gns.fred.model.FrNumber {
 		return this.frNumber.compareTo(frNumber.getFrNumber());
 	}
 
-	/*public boolean equals(Object o) {
-		return o instanceof FrNumber && ((FrNumber)o).frId.equals(frId);
+	public String toString() {
+		return frNumber;
 	}
-	
-	public int hashCode() {
-		return 353 * frId;
-	}*/
 }
