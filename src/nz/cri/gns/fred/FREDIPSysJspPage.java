aabb9c89ef98dedf6829b4c6af104d5d627cd29d
Old User <developer@gns.cri.nz>
2008-12-19 02:00:19 +0000

    watermark formatting
    git-svn-id: file:///repo/applications/FRED/trunk@11978 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- src/nz/cri/gns/fred/FREDIPSysJspPage.java
+++ src/nz/cri/gns/fred/FREDIPSysJspPage.java
@@ -90,7 +90,7 @@ public abstract class FREDIPSysJspPage extends IPSysJspPage {
 		
 		//set FRNumber lik
 		String htmlLink = "<img src=\"images\\blank.gif\" height=\"20\" width=\"10\" alt=\"\" /><form method=\"post\" action=\"locality\" name=\"FRNumJumpForm\" style=\"display: inline; margin: 0;\">"
-			+ "<input type=\"text\" size=\"10\" id=\"frTBox\" name=\"frNum\" style=\"border: 0; font-size: 10pt; color: #666666;\" value=\"Enter FR Num\" onFocus=\"clearTextbox('frTBox','Enter FR Num');\" onBlur=\"showDefaultText('frTBox','Enter FR Num');\" />&nbsp;"
+			+ "<input type=\"text\" size=\"12\" id=\"frTBox\" name=\"frNum\" style=\"border: 0; font-size: 10pt; color: #666666;\" value=\"Enter FR Num\" onFocus=\"clearTextbox('frTBox','Enter FR Num');\" onBlur=\"showDefaultText('frTBox','Enter FR Num');\" />&nbsp;"
 			+ "<input type=\"submit\" value=\"Go\" /></form>";
 		et.setButtons(new Link[] {
 				new CustomHTMLLink(htmlLink)
