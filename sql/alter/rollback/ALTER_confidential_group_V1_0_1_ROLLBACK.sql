9fc2da92e727f96535f656a89d99ed884af3731b
Old User <developer@gns.cri.nz>
2010-11-09 05:18:23 +0000

    Incorrect syntax in alter script corrected.
    git-svn-id: file:///repo/applications/FRED/branches/FY_2010_2011@20433 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- sql/alter/rollback/ALTER_confidential_group_V1_0_1_ROLLBACK.sql
+++ sql/alter/rollback/ALTER_confidential_group_V1_0_1_ROLLBACK.sql
@@ -2,6 +2,5 @@ ALTER TABLE confidential_group ADD (
 	owner_id NUMBER(5,0)
 );
 
-ALTER TABLE confidential_group ADD CONSTRAINT (
-	confid_group_fk2 FOREIGN KEY (owner_id) REFERENCES ip.person(pe_id)
-);
+ALTER TABLE confidential_group ADD CONSTRAINT 
+	confid_group_fk2 FOREIGN KEY (owner_id) REFERENCES ip.person(pe_id);
