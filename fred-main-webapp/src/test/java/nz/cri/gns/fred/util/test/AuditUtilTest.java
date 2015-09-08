4967a77bdb09cadfa3ec3db692e2cb324accd4ef
Peck Choo Tan <p.tan@gns.cri.nz>
2015-09-08 00:19:49 +0000

    Remove unused imports
    git-svn-id: file:///repo/applications/FRED/trunk@33100 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- fred-main-webapp/src/test/java/nz/cri/gns/fred/util/test/AuditUtilTest.java
+++ fred-main-webapp/src/test/java/nz/cri/gns/fred/util/test/AuditUtilTest.java
@@ -1,22 +1,6 @@
 package nz.cri.gns.fred.util.test;
 
-import java.sql.SQLException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-import net.sf.hibernate.HibernateException;
-import net.sf.hibernate.SessionFactory;
-import net.sf.hibernate.cfg.Configuration;
-import net.sf.hibernate.cfg.Environment;
-import net.sf.hibernate.dialect.Oracle9Dialect;
-import net.sf.hibernate.odmg.Name;
-import nz.cri.gns.dataaccess.StorageAccessException;
-import nz.cri.gns.dbcp.GOLDataSourceFactory;
 import nz.cri.gns.fred.dao.DAOFactory;
-import nz.cri.gns.fred.hibernate.util.FredHibernate;
-import nz.cri.gns.fred.util.AuditUtil;
-import junit.framework.TestCase;
 
 public class AuditUtilTest {
 
