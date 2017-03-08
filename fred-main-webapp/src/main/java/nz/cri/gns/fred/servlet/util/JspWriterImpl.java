632867f957db8e718317a28f37c8225a705ceba2
Old User <developer@gns.cri.nz>
2017-03-08 03:19:45 +0000

    More for the latest snapshot. de.jsp is now De_jsp.java
    git-svn-id: file:///repo/applications/FRED/branches/adoptedAge@40001 976ac4c7-77c1-46cc-9d05-16a3bd20634c

---
+++ fred-main-webapp/src/main/java/nz/cri/gns/fred/servlet/util/JspWriterImpl.java
@@ -0,0 +1,150 @@
+package nz.cri.gns.fred.servlet.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import javax.servlet.jsp.JspWriter;
+
+/** I can't find an implementation so I quickly made one for use in Servlets ported from bad JSP code.
+ * After you've finished writing stuff, you need to call >>flush(). The PrintWriter has a buffer on it. */
+public class JspWriterImpl extends JspWriter {
+    private OutputStream out;
+    private PrintWriter pw;
+    
+    public JspWriterImpl(OutputStream o) {
+        super(0, false); // We don't use any of this.
+        out = o;
+        pw = new PrintWriter(out, false);
+    }
+    
+    @Override
+    public void newLine() throws IOException {
+        pw.println();
+    }
+
+    @Override
+    public void print(boolean b) throws IOException {
+        pw.print(b);
+    }
+
+    @Override
+    public void print(char c) throws IOException {
+        pw.print(c);
+    }
+
+    @Override
+    public void print(int i) throws IOException {
+        pw.print(i);
+    }
+
+    @Override
+    public void print(long l) throws IOException {
+        pw.print(l);
+    }
+
+    @Override
+    public void print(float f) throws IOException {
+        pw.print(f);
+    }
+
+    @Override
+    public void print(double d) throws IOException {
+        pw.print(d);
+    }
+
+    @Override
+    public void print(char[] s) throws IOException {
+        pw.print(s);
+    }
+
+    @Override
+    public void print(String s) throws IOException {
+        pw.print(s);
+    }
+
+    @Override
+    public void print(Object obj) throws IOException {
+        pw.print(obj);
+    }
+
+    @Override
+    public void println() throws IOException {
+        pw.println();
+    }
+
+    @Override
+    public void println(boolean x) throws IOException {
+        pw.println(x);
+    }
+
+    @Override
+    public void println(char x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(int x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(long x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(float x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(double x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(char[] x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(String x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void println(Object x) throws IOException {
+        pw.print(x);
+    }
+
+    @Override
+    public void clear() throws IOException {
+        
+    }
+
+    @Override
+    public void clearBuffer() throws IOException {
+        
+    }
+
+    @Override
+    public void flush() throws IOException {
+        pw.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        pw.close();
+    }
+
+    @Override
+    public int getRemaining() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        pw.write(cbuf, off, len);
+    }
+
+}
