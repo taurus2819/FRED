c03e8b4575ed59827b0f863d0953945fdee6b494
Old User <developer@gns.cri.nz>
2018-03-02 01:31:07 +0000

    IS-808 Removed IR_ID from fr_user_view (SQL supplied by Richard)
    git-svn-id: file:///repo/applications/FRED/trunk@43026 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- fred-main-webapp/src/main/sql/view/fr_user_view.sql
+++ fred-main-webapp/src/main/sql/view/fr_user_view.sql
@@ -1,15 +1,21 @@
-CREATE OR REPLACE VIEW FR_User_View AS
-select
+-- Remove IR_ID column from fr_user_view
+create or replace force view fr.fr_user_view ("PE_ID", "GIVEN_NAME", "FAMILY_NAME", "FULL_NAME", "PE_CLIENT_CODE", "CL_COMPANY_NAME", "DELETED") as
+  select
+/**
+ * $HeadURL$
+ * $LastChangedBy$
+ * $Rev$
+ * $Date$
+ */
    person_id as pe_id
    , given_name
    , family_name
    , full_name
    , pe_client_code
    , cl_company_name
-   , min( decode ir_name, 'FR_DATA_ENTRY', 2, 'FR_WEBSITE_ACCESS', 4) as ir_id
    , deleted
-from ip.user_right_view pv
-where ir_name in ('FR_DATA_ENTRY', 'FR_WEBSITE_ACCESS')
+from ip.user_right_view2 pv
+where ir_name in ('FR_DATA_ENTRY', 'FR_WEBSITE_ACCESS', 'FR_ADMIN')
 group by
    person_id
    , given_name
@@ -17,4 +23,9 @@ group by
    , full_name
    , pe_client_code
    , cl_company_name
-   , deleted
+   , deleted;
+
+-- These were invalid after updating the above...possibly were already invalid.
+alter view fr.folder_view compile;
+alter view fr.audit_view compile;
+alter package fr.fred_maintain compile;
