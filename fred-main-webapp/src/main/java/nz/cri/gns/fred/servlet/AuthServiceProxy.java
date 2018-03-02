1b0020dd7938152dfe699ab7b09a90ce440a9f1a
Old User <developer@gns.cri.nz>
2018-03-02 00:14:30 +0000

    IS-808 Added permission check to auth service proxy.
    git-svn-id: file:///repo/applications/FRED/trunk@43022 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- fred-main-webapp/src/main/java/nz/cri/gns/fred/servlet/AuthServiceProxy.java
+++ fred-main-webapp/src/main/java/nz/cri/gns/fred/servlet/AuthServiceProxy.java
@@ -6,7 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import nz.cri.gns.auth.AuthServiceException;
+import nz.cri.gns.auth.security.IpGrantedAuthority;
+import static nz.cri.gns.fred.FredGrantedAuthorities.FR_ADMIN;
 import nz.cri.gns.fred.util.UserUtil;
+import nz.cri.gns.jsp.JspUtils;
 
 /**
  *
@@ -16,6 +19,10 @@ public class AuthServiceProxy extends HttpServlet {
  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!JspUtils.userIsAuthorized(JspUtils.getUser(request.getSession()), new IpGrantedAuthority(FR_ADMIN))) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
         try {
             UserUtil.updateUserRight(
                     request.getParameter("action"),
