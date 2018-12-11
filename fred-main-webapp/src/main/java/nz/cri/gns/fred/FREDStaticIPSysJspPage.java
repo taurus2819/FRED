2812a1a47bdc4175c497dfea72df0472e8b0b4a2
Old User <developer@gns.cri.nz>
2018-12-11 04:07:19 +0000

    Add User Manual. PDB-183
    git-svn-id: file:///repo/applications/FRED/trunk@44818 976ac4c7-77c1-46cc-9d05-16a3bd20634c

--- fred-main-webapp/src/main/java/nz/cri/gns/fred/FREDStaticIPSysJspPage.java
+++ fred-main-webapp/src/main/java/nz/cri/gns/fred/FREDStaticIPSysJspPage.java
@@ -19,15 +19,16 @@ public abstract class FREDStaticIPSysJspPage extends FREDIPSysJspPage{
 		
 		//add nav
 		IconnedLink[] il = new IconnedLink[8];
-		il[0] = new IconnedLink("quick_start.jsp", "images/book.gif", "Quick Start");
-		il[1] = new IconnedLink("about.jsp", "images/book.gif", "About");
-		il[2] = new IconnedLink("contacts.jsp", "images/register.gif", "Contacts");
-		il[3] = new IconnedLink("faq.jsp", "images/help.gif", "FAQ");
-		il[4] = new IconnedLink("whats_new.jsp", "images/book.gif", "Whats New");
-		il[5] = new IconnedLink("http://data.gns.cri.nz/register/user_reg.jsp?DBase=FRED", "images/register.gif", "Register for FRED account");
-		il[6] = new IconnedLink("conditions.jsp", "images/tc.gif", "Conditions of Use");
+		il[0] = new IconnedLink("user_manual.pdf", "images/book.gif", "FRED User Manual");
+		il[1] = new IconnedLink("quick_start.jsp", "images/book.gif", "Quick Start");
+		il[2] = new IconnedLink("about.jsp", "images/book.gif", "About");
+		il[3] = new IconnedLink("contacts.jsp", "images/register.gif", "Contacts");
+		il[4] = new IconnedLink("faq.jsp", "images/help.gif", "FAQ");
+		il[5] = new IconnedLink("whats_new.jsp", "images/book.gif", "Whats New");
+		il[6] = new IconnedLink("http://data.gns.cri.nz/register/user_reg.jsp?DBase=FRED", "images/register.gif", "Register for FRED account");
+		il[7] = new IconnedLink("conditions.jsp", "images/tc.gif", "Conditions of Use");
 		//il[7] = new IconnedLink("download.jsp", "images/save.gif", "Downloads");
-		il[7] = new IconnedLink("http://data.gns.cri.nz/staff/email.jsp?id=frf&subject=FRF%20Feeedback", "images/register.gif", "Feedback");
+		il[8] = new IconnedLink("http://data.gns.cri.nz/staff/email.jsp?id=frf&subject=FRF%20Feeedback", "images/register.gif", "Feedback");
 		addButtons(et, il);
 		
 		return et;
