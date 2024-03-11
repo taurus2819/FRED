5d7624ee8bd080496a4b1bef4e7af16f9c89db15
Lockyer <g.lockyer@gns.cri.nz>
2024-03-12 09:47:29 +1300

    FrontEnd Validate co-ordinates
    Require that valid co-ordinates have been provided before allowing
    records to be saved.
    
    Remove the Chatam Island, Auckland, Campbell Island and New Caledonia
    map grids.
    
    Add validation on the Easting and Northing values.
    
    Signed-off-by: Gary Lockyer <g.lockyer@gns.cri.nz>

--- fred-main-webapp/src/main/webapp/scripts/dataentry.js
+++ fred-main-webapp/src/main/webapp/scripts/dataentry.js
@@ -18,6 +18,7 @@ function requiredInput(inputName, errorRow, errorCell, message) {
             }
         }
         updateSubmit();
+        updateSave();
     });
     input.addEventListener("invalid", function(e) {
         document.getElementById(inputName).setCustomValidity(message);
@@ -39,9 +40,174 @@ function updateSubmit() {
     }
 }
 
+function updateSave() {
+    const save = document.getElementById("save-button");
+    if (save === null) {
+        return;
+    }
+    const invalid = document.querySelectorAll("input:invalid, textarea:invalid");
+    let validCoordinates = true;
+    for (let i = 0; i < invalid.length; i++) {
+        let id = invalid[i].id;
+        if (id === "eastInput" || id === "northInput" ||
+            id === "coordTypeInput" || id === "MapSheetInput") {
+            // Valid co-ordinates are required before the record can be
+            // saved
+            validCoordinates = false;
+            save.setAttribute("hidden", "hidden");
+            document.getElementById("coordRequiredMessage").removeAttribute("hidden");
+            return;
+        }
+    }
+    save.removeAttribute("hidden");
+    document.getElementById("coordRequiredMessage").setAttribute("hidden", "hidden");
+}
+
 function checkOnLoad() {
+    // Add a leading "+" sign to the Longitude
+    // if it's not there, the plus sign gets dropped on the round trip to/from
+    // the backend.
+    const box = document.getElementById("coordTypeInput");
+	switch (box.selectedIndex) {
+		case 7:
+		case 8:
+		case 9:
+        case 10:
+            const element = document.getElementById("northInput");
+            const value   = element.value;
+            if (value && !value.startsWith("+") && !value.startsWith("-")) {
+                element.value = "+" + value;
+            }
+	}
+    // Once the data has been adjusted, validate the required
+    // fields.
     const required = document.querySelectorAll("[Required]");
     for (let i=0; i < required.length; i++) {
         required[i].checkValidity();
     }
+    updateSave();
+    updateSubmit();
+}
+
+const NZMG_EASTING_VALIDATION = {
+    pattern: /^\d{7}(\.\d)?$/,
+    error: " must be a number between 0 and 9999999",
+    min: 0,
+    max: 9999999
+};
+const NZMG_NORTHING_VALIDATION = {
+    pattern: /^\d{7}(\.\d)?$/,
+    error: " must be a number between 0 and 9999999",
+    min: 0,
+    max: 9999999
+};
+const NZTM_EASTING_VALIDATION = {
+    pattern: /^\d{7}(\.\d)?$/,
+    error: " must be a number between 0 and 9999999",
+    min: 0,
+    max: 9999999
+};
+const NZTM_NORTHING_VALIDATION = {
+    pattern: /^\d{7}(\.\d)?$/,
+    error: " must be a number between 0 and 9999999",
+    min: 0,
+    max: 9999999
+};
+
+const MS_EASTING_VALIDATION = {
+    pattern: /^\d{3,4}$/,
+    error: " must be a number between 0 and 9999",
+    min: 0,
+    max: 9999
+};
+const MS_NORTHING_VALIDATION = {
+    pattern: /^\d{3,4}$/,
+    error: " must be a number between 0 and 9999",
+    min: 0,
+    max: 9999
+};
+
+const LATITUDE_VALIDATION = {
+    pattern: /^[-+]?\d{1,3}(\.\d{0,4})?$/,
+    error: " must be a number between -179.9999 and 180.0000",
+    min: -180,
+    max: 180
+};
+const LONGITUDE_VALIDATION = {
+    pattern: /^[-+].\d{1,2}(\.\d{0,4})?$/,
+    error: " must be a number between -90 and +90 and sign is required",
+    min: -90,
+    max: 90
+};
+
+function validateEasting(inputName, errorRow, errorCell) {
+    let rules = new Map();
+    rules.set("NZMG", NZMG_EASTING_VALIDATION);
+    rules.set("NZMS260", MS_EASTING_VALIDATION);
+    rules.set("NZTM", NZTM_EASTING_VALIDATION);
+    rules.set("NZTopo50", MS_EASTING_VALIDATION);
+    rules.set("NZ Yard SthIsl", MS_EASTING_VALIDATION);
+    rules.set("NZ Yard NthIsl", MS_EASTING_VALIDATION);
+    rules.set("NZMS1 SthIsl", MS_EASTING_VALIDATION);
+    rules.set("NZMS1 NthIsl", MS_EASTING_VALIDATION);
+    rules.set("NZGD49", LATITUDE_VALIDATION);
+    rules.set("Chatham Island Datum", LATITUDE_VALIDATION);
+    rules.set("NZGD2000", LATITUDE_VALIDATION);
+    rules.set("WGS84", LATITUDE_VALIDATION);
+
+    input = document.getElementById(inputName);
+    input.addEventListener(
+        "change",
+        getCordinateValidationFunction(
+            inputName, errorRow, errorCell, "easting", rules));
+}
+
+function validateNorthing(inputName, errorRow, errorCell) {
+    let rules = new Map();
+    rules.set("NZMG", NZMG_NORTHING_VALIDATION);
+    rules.set("NZMS260", MS_NORTHING_VALIDATION);
+    rules.set("NZTM", NZTM_NORTHING_VALIDATION);
+    rules.set("NZTopo50", MS_EASTING_VALIDATION);
+    rules.set("NZ Yard SthIsl", MS_NORTHING_VALIDATION);
+    rules.set("NZ Yard NthIsl", MS_NORTHING_VALIDATION);
+    rules.set("NZMS1 SthIsl", MS_NORTHING_VALIDATION);
+    rules.set("NZMS1 NthIsl", MS_NORTHING_VALIDATION);
+    rules.set("NZGD49", LONGITUDE_VALIDATION);
+    rules.set("Chatham Island Datum", LONGITUDE_VALIDATION);
+    rules.set("NZGD2000", LONGITUDE_VALIDATION);
+    rules.set("WGS84", LONGITUDE_VALIDATION);
+
+    input = document.getElementById(inputName);
+    input.addEventListener(
+        "change",
+        getCordinateValidationFunction(
+            inputName, errorRow, errorCell, "northing", rules));
+}
+
+function getCordinateValidationFunction(
+        inputName, errorRow, errorCell, label, rules) {
+    return function(ev) {
+        const el = document.getElementById(inputName);
+        const name  = document.getElementById(label).innerHTML;
+        const value = el.value;
+        const type  = document.getElementById("coordTypeInput").value;
+        const rule  = rules.get(type);
+        if (!value || !value.trim()) {
+            const message = "A value is required for " + name;
+            el.setCustomValidity(message);
+            document.getElementById(errorCell).innerHTML = message;
+            document.getElementById(errorRow).removeAttribute("hidden");
+        }
+        else if (!rule.pattern.test(value) || value < rule.min || value > rule.max) {
+            const message = "For " + type +", " + name + rule.error;
+            el.setCustomValidity(message);
+            document.getElementById(errorCell).innerHTML = message;
+            document.getElementById(errorRow).removeAttribute("hidden");
+        } else {
+            el.setCustomValidity("");
+            document.getElementById(errorRow).setAttribute("hidden", "hidden");
+        }
+        updateSubmit();
+        updateSave();
+    };
 }
