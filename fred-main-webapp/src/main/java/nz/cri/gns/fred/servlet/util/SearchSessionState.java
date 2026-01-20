d4d0802bf184ba3a177608ac4e06885cbf37743b
sitikond <p.sitikond@gns.cri.nz>
2026-01-20 20:49:42 +1300

    PDB-316 : copying all the saved sessions into an unmodifiable collectiosn for restoring

--- fred-main-webapp/src/main/java/nz/cri/gns/fred/servlet/util/SearchSessionState.java
+++ fred-main-webapp/src/main/java/nz/cri/gns/fred/servlet/util/SearchSessionState.java
@@ -4,6 +4,8 @@
  */
 package nz.cri.gns.fred.servlet.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpSession;
@@ -17,22 +19,40 @@ public class SearchSessionState {
     private static final String SAMPLE_IDS_KEY = "FRED.samples";
     private static final String FEATURE_IDS_KEY = "FRED.features";
     private static final String QUERY_STRING_KEY = "FRED.queryString";
-    
-    private SearchSessionState(){
-        
+       
+    private SearchSessionState(){        
     }
 
     // Store search results in user's session
     public static void save(HttpSession session, List<Integer> featureIds, List<Integer> sampleIds, String queryString){
+        session.setAttribute(FEATURE_IDS_KEY, immutableCopy(featureIds));
+        session.setAttribute(SAMPLE_IDS_KEY, sampleIds == null ? null : immutableCopy(sampleIds));
+        session.setAttribute(QUERY_STRING_KEY, queryString);
         
     }
     
+    //restor the saved resulots from the session
     public static Optional<Snapshot> restore(HttpSession session){
-        return Optional.empty();
+        List<Integer> featureIds = immutableCopy((List<Integer>) session.getAttribute(FEATURE_IDS_KEY));
+        String queryString = (String) session.getAttribute(QUERY_STRING_KEY);
+        
+        if(featureIds == null || featureIds.isEmpty() || queryString == null){
+            return Optional.empty();
+        }
+        
+        List<Integer> sampleIds = immutableCopy((List<Integer>) session.getAttribute(SAMPLE_IDS_KEY));
+        return Optional.of(new Snapshot(featureIds, sampleIds, queryString));
+    }
+    
+    private static List<Integer> immutableCopy(List<Integer> sources) {
+        if(sources == null){
+            return null;
+        }
+        return Collections.unmodifiableList(sources);
     }
     
     public static final class Snapshot {
-     private final List<Integer> featureIds;
+        private final List<Integer> featureIds;
         private final List<Integer> sampleIds;
         private final String queryString;
 
@@ -56,8 +76,7 @@ public class SearchSessionState {
 
         public String getQueryString() {
             return queryString;
-        }
-    
+        }    
 }
     
     
